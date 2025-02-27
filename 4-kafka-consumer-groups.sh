2c34196c70ab90ad346678df06f3aee8415859a2
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-02-27 12:57:56 +0900

    Feat: Consumer Group으로 topic 소비

--- 4-kafka-consumer-groups.sh
+++ 4-kafka-consumer-groups.sh
@@ -43,16 +43,16 @@ kafka-consumer-groups.sh --command-config playground.config --bootstrap-server c
 # documentation for the command 
 kafka-consumer-groups.sh 
 
-# list consumer groups
+# list consumer groups 
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list
  
-# describe one specific group
+# describe one specific group - my-second-application의 상세 정보 출력
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-second-application
 
-# describe another group
+# describe another group - my-first-application의 상세 정보 출력
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
 
-# start a consumer
+# start a consumer - group my-first-application으로 시작 (producer로부터 메시지를 받음) / kafka-console-consumer로 실행
 kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
 
 # describe the group now
@@ -61,7 +61,7 @@ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my
 # describe a console consumer group (change the end number)
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group console-consumer-10592
 
-# start a console consumer
+# start a console consumer 
 kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
 
 # describe the group again
